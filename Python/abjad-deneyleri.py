0290a152d14fdd91b5d83a752d1244b99b88e2a0
Abdil <90824488+metatronslove@users.noreply.github.com>
2024-11-09 19:47:59 +0300

    Update abjad-deneyleri.py

--- Python/abjad-deneyleri.py
+++ Python/abjad-deneyleri.py
@@ -2027,7 +2027,7 @@ def numerolog(metin, tablo='tr', outas='tam', shadda=1):
 		traceback.print_exc()
 # ----------------------------------------------------------------------
 methods = [1,7,12,17,22,27,32]
-testers = [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000,6447] #6394 is a test entitie ;)
+testers = [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000,6447] #6447 is a test entitie ;)
 methlen = 2
 testlen = 6
 hadimln = 10
@@ -2045,4 +2045,4 @@ for method in methods:
 			strline = "method: " + methodview[-1*methlen:] + " \033[92m►\033[0mtester: " + testerview[-1*testlen:] + " \033[92m►\033[0mabjad: " + abjad_view[-1*testlen:] + " \033[92m►\033[0mhuddam: " + huddam_len[-1*hadimln:] + " \033[92m►\033[0mresult: \033[92mcorrect match\033[0m"
 			print(strline, sep='',end =' '*19 + '\b'*len(strline) + '\b'*19, file = sys.stdout, flush = True)
 			time.sleep(.043)
-print('\033[92mtest complete\033[0m', sep='',end ='\n\r', file = sys.stdout, flush = True)
+print('\033[92mtest complete\033[0m', sep='',end =' '*len(strline) + '\b'*len(strline) +'\n\r', file = sys.stdout, flush = True)
